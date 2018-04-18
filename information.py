fd9bf9914c23a0dd1299aa4b8395f684fca71932
lin <1031562591@qq.com>
2018-04-18 15:37:02 +0800

    创建redis，并进行配置

--- information.py
+++ information.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
 app = Flask(__name__)
 
 class Config(object):
@@ -7,8 +8,15 @@ class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+#     redis 配置
+    REDIS_HOST = '127.0.0.1'
+    REDIS_POST = 6379
+
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_POST)
 
 @app.route('/')
 def hello_world():

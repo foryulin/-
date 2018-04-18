be9ccb4510c3dcdf791c84cf0a97690388091d8d
lin <1031562591@qq.com>
2018-04-18 15:28:57 +0800

    创建config类并连接到mysql数据库

--- information.py
+++ information.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
